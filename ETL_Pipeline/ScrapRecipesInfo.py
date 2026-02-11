62b446039e199e5ad9f7e649b518cf191dae27be
Alexis Aguilar <alexis.uaguilaru@gmail.com>
2026-02-10 18:57:26 -0600

    Add script to scrape specific information about one recipe

---
+++ ETL_Pipeline/ScrapRecipesInfo.py
@@ -0,0 +1,75 @@
+from crawl4ai import AsyncWebCrawler , CrawlerRunConfig , CrawlResult , JsonCssExtractionStrategy
+
+async def MainScrapRecipeInformation(RecipeLink: str):
+    SessionID = 'Session_RecipeInformation'
+
+    ExtractionSchema_TimesServings = {
+        'name': 'Recipe Information about Times and Serving',
+        'baseSelector': 'div.mm-recipes-details__item',
+        'fields': [
+            {'name': 'Label', 'selector': '.mm-recipes-details__label', 'type': 'text'},
+            {'name': 'Value', 'selector': '.mm-recipes-details__value', 'type': 'text'},
+        ]
+    }
+
+    ExtractionSchema_Ingredients = {
+        'name': 'Recipe Information about Ingredients and Quantities',
+        'baseSelector': 'ul.mm-recipes-structured-ingredients__list',
+        'fields': [
+            {'name': 'Ingredients', 'selector': 'li', 'type': 'list', 'fields':[{'name': 'ingredient', 'type': 'text'}]},
+        ]
+    }
+
+    ExtractionSchema_Directions = {
+        'name': 'Recipe Information about Directions',
+        'baseSelector': 'ol.comp.mntl-sc-block.mntl-sc-block-startgroup.mntl-sc-block-group--OL',
+        'fields': [
+            {'name': 'Steps', 'selector': 'li', 'type': 'list', 'fields':[{'name': 'step', 'type': 'text'}]},
+        ]
+    }
+
+    ExtractionSchema_NutritionalFacts = {
+        'name': 'Recipe Information about Nutritional Facts',
+        'baseSelector': 'table.mm-recipes-nutrition-facts-label__table',
+        'fields': [
+            {'name': 'Calories', 'selector': 'tr.mm-recipes-nutrition-facts-label__calories span:nth-of-type(2)', 'type': 'text'},
+            {'name': 'Nutritional Facts', 'selector': 'tbody tr:nth-child(n+2)', 'type': 'nested_list', 'fields':[
+                {'name': 'nutrition label', 'selector': 'td span', 'type': 'text'},
+                {'name': 'nutrition value', 'selector': 'td', 'type': 'regex', 'pattern': r'[A-Za-z ]+\s*([\d.]+\w+)'},
+            ]}
+        ]
+    }
+
+    CrawlerConfig = CrawlerRunConfig(
+        session_id = SessionID,
+    )
+
+    async with AsyncWebCrawler() as crawler:
+
+        CrawlerConfig.extraction_strategy = JsonCssExtractionStrategy(ExtractionSchema_TimesServings)
+        result_TimesServings: CrawlResult  = await crawler.arun(
+            url = RecipeLink,
+            config = CrawlerConfig,
+        )
+        print(result_TimesServings.extracted_content)
+
+        CrawlerConfig.extraction_strategy = JsonCssExtractionStrategy(ExtractionSchema_Ingredients)
+        result_Ingredients: CrawlResult  = await crawler.arun(
+            url = RecipeLink,
+            config = CrawlerConfig,
+        )
+        print(result_Ingredients.extracted_content)
+
+        CrawlerConfig.extraction_strategy = JsonCssExtractionStrategy(ExtractionSchema_Directions)
+        result_Directions: CrawlResult  = await crawler.arun(
+            url = RecipeLink,
+            config = CrawlerConfig,
+        )
+        print(result_Directions.extracted_content)
+
+        CrawlerConfig.extraction_strategy = JsonCssExtractionStrategy(ExtractionSchema_NutritionalFacts)
+        result_NutritionalFacts: CrawlResult  = await crawler.arun(
+            url = RecipeLink,
+            config = CrawlerConfig,
+        )
+        print(result_NutritionalFacts.extracted_content)
