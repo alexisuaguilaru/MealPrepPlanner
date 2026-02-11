6ac55ccd62c04b64e55ac9add5ab5ec75f93510d
Alexis Aguilar <alexis.uaguilaru@gmail.com>
2026-02-10 18:56:44 -0600

    Add script to scrape a list of recipes from a cuisine

---
+++ ETL_Pipeline/ScrapRecipes.py
@@ -0,0 +1,22 @@
+from crawl4ai import AsyncWebCrawler , CrawlerRunConfig , CrawlResult , JsonCssExtractionStrategy
+
+async def MainScrapRecipesFromCuisine(CuisineLink: str):
+    ExtractionSchema = {
+        'name': 'Recipes from a Cuisine',
+        'baseSelector': 'a.comp.mntl-card-list-items.mntl-universal-card.mntl-document-card.mntl-card.card.card--no-image',
+        'fields': [
+            {'name': 'Recipe Name', 'selector': 'span.card__title-text', 'type': 'text'},
+            {'name': 'Link', 'type': 'attribute', 'attribute': 'href'}
+        ]
+    }
+
+    CrawlerConfig = CrawlerRunConfig(
+        extraction_strategy = JsonCssExtractionStrategy(ExtractionSchema),
+    )
+
+    async with AsyncWebCrawler() as crawler:
+        result: CrawlResult  = await crawler.arun(
+            url = CuisineLink,
+            config = CrawlerConfig,
+        )
+        print(result.extracted_content)
