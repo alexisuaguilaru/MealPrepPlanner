539345918ca023e85cd49778fb8e124387568985
Alexis Aguilar <alexis.uaguilaru@gmail.com>
2026-03-03 19:34:49 -0600

    Add function and paths to save/dump extracted information of a recipe from allrecipes page

--- ETL_Pipeline/Recipes/Allrecipes/ScrapeMain.py
+++ ETL_Pipeline/Recipes/Allrecipes/ScrapeMain.py
@@ -1,12 +1,17 @@
+from pathlib import Path
 import asyncio
 
 from .ScrapeRecipes import MainScrapeRecipesFromCuisine
 from .ScrapeRecipesInfo import MainScrapeRecipeInformation
 from .CleanRecipeInfo import CleanRecipe
+from .SaveRecipe import SaveCleanRecipeInfo
 
 def MainScraping():
+    RecipesPath = Path('Datasets/Recipes/Allrecipes/')
+    RecipesPath.mkdir(parents=True,exist_ok=True)
+
     MexicanRecipes = asyncio.run(MainScrapeRecipesFromCuisine('https://www.allrecipes.com/recipes/728/world-cuisine/latin-american/mexican/'))
-    for mexican_recipe in MexicanRecipes[5:6]:
+    for mexican_recipe in MexicanRecipes[4:8]:
         recipe_name:str = mexican_recipe['Recipe Name']
         recipe_link = mexican_recipe['Link']
         recipe_info = asyncio.run(MainScrapeRecipeInformation(recipe_link))
@@ -15,4 +20,4 @@ def MainScraping():
             clean_recipe_info = CleanRecipe(recipe_info)
             clean_recipe_info['Name'] = recipe_name
             clean_recipe_info['Source'] = recipe_link
-            yield clean_recipe_info
+            yield SaveCleanRecipeInfo(clean_recipe_info,RecipesPath)
