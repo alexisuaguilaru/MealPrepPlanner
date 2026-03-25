b22546eea97d38cad3b99491c38577de9971338e
Alexis Aguilar <alexis.uaguilaru@gmail.com>
2026-03-25 00:06:49 -0600

    Modifications at scraping method to extract recipe information with xpath

--- ETL_Pipeline/Recipes/EatRight/ScrapeRecipeInfo.py
+++ ETL_Pipeline/Recipes/EatRight/ScrapeRecipeInfo.py
@@ -1,40 +1,20 @@
 import asyncio
 import random
 import json
-from crawl4ai import AsyncWebCrawler , BrowserConfig , CrawlerRunConfig , CrawlResult , JsonCssExtractionStrategy
+from crawl4ai import AsyncWebCrawler , BrowserConfig , CrawlerRunConfig , CrawlResult , JsonXPathExtractionStrategy
 
 from ...Utils import BasicBrowserConfig , BasicCrawlerRunConfig
 
-FieldWithTimes = [
-    {'name': 'Times', 'selector': 'p:nth-of-type(2)', 'type': 'html'},
-    {'name': 'Ingredients', 'selector': 'p:nth-of-type(3)', 'type': 'html'},
-    {'name': 'Servings', 'selector': 'p:nth-of-type(5)', 'type': 'text'},
-    {'name': 'Nutritional Facts', 'selector': 'p:nth-of-type(6)', 'type': 'text'},
-    {'name': 'Directions', 'selector': 'ol li', 'type': 'list', 'fields': [{'name': 'direction', 'type': 'text'}]},
-]
-
-FieldWithOutTimes = [
-    {'name': 'Ingredients', 'selector': 'p:nth-of-type(2)', 'type': 'html'},
-    {'name': 'Servings', 'selector': 'p:nth-of-type(4)', 'type': 'text'},
-    {'name': 'Nutritional Facts', 'selector': 'p:nth-of-type(5)', 'type': 'text'},
-    {'name': 'Directions', 'selector': 'ol li', 'type': 'list', 'fields': [{'name': 'direction', 'type': 'text'}]},
-]
-
 async def MainScrapeRecipeInformation(RecipeLink: str):
-    ExtractionSchema_TypeExtraction = {
-        'name': 'List of Paragraphs',
-        'baseSelector': 'section.rtf',
-        'fields': [
-            {'name': 'Paragraphs', 'selector': 'p', 'type': 'list', 'fields': [
-                {'name': 'paragraph', 'type': 'text'}
-            ]},
-        ]
-    }
-
     ExtractionSchema = {
         'name': 'List of Mealtimes',
         'baseSelector': 'section.rtf',
-        'fields': None
+        'fields': [
+            {'name': 'Times', 'selector': ".//p[contains(text(),'minute') or contains(text(), 'hour')]", 'type': 'text', 'default': '0 minutes'},
+            {'name': 'Ingredientes', 'selector': ".//h3[contains(text(),'Ingredient')]/following-sibling::p[1]", 'type': 'text'},
+            {'name': 'Servings Nutritional Facts', 'selector': ".//h3[contains(text(), 'Nutrition')]/following-sibling::p", 'type': 'html'},
+            {'name': 'Directions', 'selector': "//ol/li", 'type': 'list', 'fields': [{'name': 'direction', 'type': 'text'}]},
+        ],
     }
 
     Browser = BrowserConfig(
@@ -44,23 +24,11 @@ async def MainScrapeRecipeInformation(RecipeLink: str):
     CrawlerConfig_EatRight = BasicCrawlerRunConfig.copy()
     del CrawlerConfig_EatRight['wait_until']
     CrawlerConfig = CrawlerRunConfig(
-        extraction_strategy = JsonCssExtractionStrategy(ExtractionSchema_TypeExtraction),
+        extraction_strategy = JsonXPathExtractionStrategy(ExtractionSchema),
         **CrawlerConfig_EatRight,
     )
 
     async with AsyncWebCrawler(config=Browser) as crawler:
-        result_TypeExtraction: CrawlResult  = await crawler.arun(
-            url = RecipeLink,
-            config = CrawlerConfig,
-        )
-
-        json_type_extraction = json.loads(result_TypeExtraction.extracted_content)
-        if len(json_type_extraction[0]['Paragraphs']) > 5:
-            ExtractionSchema['fields'] = FieldWithTimes
-        else:
-            ExtractionSchema['fields'] = FieldWithOutTimes
-
-        CrawlerConfig.extraction_strategy = JsonCssExtractionStrategy(ExtractionSchema)
         result: CrawlResult  = await crawler.arun(
             url = RecipeLink,
             config = CrawlerConfig,
