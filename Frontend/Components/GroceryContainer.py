960d4eae56baf641cf22d274d58e0a592635921f
Alexis Aguilar <alexis.uaguilaru@gmail.com>
2026-05-10 23:58:42 -0600

    Minor change at style of grocery list
    Changes include:
    - Modifications at font size of labels and values

--- Frontend/Components/GroceryContainer.py
+++ Frontend/Components/GroceryContainer.py
@@ -8,22 +8,42 @@ def GroceryListContainer():
 
     Ingredients , Selectors = st.columns([3,1])
 
+    with Selectors:
+        with st.container(vertical_alignment='center',horizontal_alignment='center'):
+            st.markdown('##### **Número de Porciones**')
+            NumServings = st.selectbox(
+                '',
+                list(range(1,1000)),
+                label_visibility = 'collapsed',
+            )
+            
+            TotalPricePerServing = (ListIngredients['TotalPrice']/ListIngredients['Servings']).sum()
+            Total = TotalPricePerServing*NumServings
+            st.markdown(
+                f'##### **Costo por Total de Porciones:**',
+                text_alignment = 'center',
+            )
+            st.markdown(
+                f'##### **${Total:.2f} MXN**',
+                text_alignment = 'center'
+            )
+
     with Ingredients:
-        Name , Amount , TotalPrice = st.columns(3,border=True)
+        Name , Amount , TotalCost = st.columns(3,border=True)
 
         with Name:
             st.markdown(
-                "###### :color[Ingrediente]{foreground='#2391ff'}",
+                "##### :color[Ingrediente]{foreground='#2391ff'}",
                 text_alignment = 'center',
             )
         with Amount:
             st.markdown(
-                "###### :color[Cantidad]{foreground='#2391ff'}",
+                "##### :color[Cantidad]{foreground='#2391ff'}",
                 text_alignment = 'center',
             )
-        with TotalPrice:
+        with TotalCost:
             st.markdown(
-                "###### :color[Precio total]{foreground='#2391ff'}",
+                "##### :color[Costo total]{foreground='#2391ff'}",
                 text_alignment = 'center',
             )
 
@@ -31,28 +51,12 @@ def GroceryListContainer():
             with Name:
                 st.markdown(ingredient['Name'].capitalize())
             with Amount:
-                amount = ingredient['Amount'] or ''
+                amount = ingredient['Amount']*NumServings or ''
                 unit = ingredient['Unit'] or ''
                 amount_unit = ' '.join([str(amount),unit])
-                st.markdown(amount_unit)
-            with TotalPrice:
-                st.markdown(f"${ingredient['TotalPrice']:.2f} MXN")
-
-    with Selectors:
-        with st.container(vertical_alignment='center',horizontal_alignment='center'):
-            NumServings = st.selectbox(
-                '**Número de Porciones**',
-                list(range(1,1000)),
-            )
-            
-            TotalPricePerServing = (ListIngredients['TotalPrice']/ListIngredients['Servings']).sum()
-            Total = TotalPricePerServing*NumServings
-
-            st.markdown(
-                f"""
-                **Costo por total de porciones:**
-
-                **${Total:.2f} MXN**
-                """,
-                text_alignment = 'center'
-            )
+                if amount_unit != ' ':
+                    st.markdown(amount_unit)
+                else:
+                    st.markdown(":color[0]{foreground='#ffffff'}")
+            with TotalCost:
+                st.markdown(f"${ingredient['TotalPrice']*NumServings:.2f} MXN")
